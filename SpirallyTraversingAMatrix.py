ce0318cb44fb9cfe7302fab92b8b02de99f8a33f
Sadhana K S <117561777+sadhana937@users.noreply.github.com>
2024-08-01 17:06:21 +0530

    Update SpirallyTraversingAMatrix.py

--- SpirallyTraversingAMatrix.py
+++ SpirallyTraversingAMatrix.py
@@ -1,4 +1,39 @@
-class Solution:
+class Solution2:
+    # Function to return a list of integers denoting spiral traversal of matrix.
+    def spirallyTraverse(self, matrix):
+        # code here
+        output = []
+        m = len(matrix)
+        n = len(matrix[0])
+        top, bottom = 0, m - 1
+        left, right = 0, n - 1
+        while left <= right and top <= bottom:
+            
+            # Traversing right
+            for i in range(left, right + 1):
+                output.append(matrix[top][i])
+            top += 1
+            
+            # Traversing down
+            for i in range(top, bottom + 1):
+                output.append(matrix[i][right])
+            right -= 1
+            
+            if top <= bottom:
+                # Traversing left
+                for i in range(right, left - 1, -1):
+                    output.append(matrix[bottom][i])
+                bottom -= 1
+            
+            if left <= right:
+                # Traversing up
+                for i in range(bottom, top - 1, -1):
+                    output.append(matrix[i][left])
+                left += 1            
+        
+        return output
+
+class Solution1:
     # Function to return a list of integers denoting spiral traversal of matrix.
     def spirallyTraverse(self, matrix):
         # code here
